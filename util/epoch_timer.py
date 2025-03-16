55f5093abcbec2befc6c5e0c0ea859b6bbb97e06
zhy617 <zhanghongyu617@gmail.com>
2025-03-16 21:23:22 +0800

    fix: add docstring to epoch_time function to clarify parameters and return values

--- util/epoch_timer.py
+++ util/epoch_timer.py
@@ -1,4 +1,12 @@
 def epoch_time(start_time, end_time):
+    """
+    calculate the elapsed time
+    Args:
+        start_time: start time
+        end_time: end time
+    Returns:
+        (int, int): elapsed minutes, elapsed seconds
+    """
     elapsed_time = end_time - start_time
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
